e350d3941f0d8ac2090f5e4ae896da1a76815460
allmight05 <166301619+allmight05@users.noreply.github.com>
2025-05-12 12:45:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="machine_translation ge to en",
+    name="machine_translation",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
